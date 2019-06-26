b5e36248a32949970606d849255b2f2c7fa6d6f2
Popelus <52213575+Popeluso@users.noreply.github.com>
2019-06-26 11:07:40 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function myFunction() {
   var x = document.getElementById("frm1");
   var text = "Hola, mi muy señor ";
-  text += x.element[0].value + ", de la casa " + x.element[1].value + ".<br>";
+  text += x.element[0] + ", de la casa " + x.element[1] + ".<br>";
   document.getElementById("demo2").innerHTML = text;
 }
